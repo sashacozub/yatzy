8b5d534f4bb20ea1917db3c2cddfc77fa9353f94
Alexander <81567588+sashacozub@users.noreply.github.com>
2022-07-01 13:41:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,20 @@ const rolledDice = [
   { id: 4, value: 0, saved: false },
 ];
 
+// Reusable function for getting a random die value
 const getRandomDieNumber = () => Math.floor(Math.random() * 6) + 1;
 
 function App() {
   const [dice, setDice] = useState(rolledDice);
+  const [currentPlayer, setCurrentPlayer] = useState(1);
+  const [rollsLeft, setRollsLeft] = useState(3);
 
+  // Roll all the dice on button press
   const rollDice = () => {
+    if (rollsLeft !== 0) {
+      setRollsLeft((prev) => prev - 1);
+    }
+
     let tempDice = [...dice];
 
     for (let i = 0; i < tempDice.length; i++) {
@@ -24,11 +32,10 @@ function App() {
       }
     }
 
-    console.log(tempDice);
-
     setDice(tempDice);
   };
 
+  // Save or unsave any number of dice to NOT reroll
   const saveUnsaveDie = (e) => {
     let tempDice = [...dice];
 
@@ -37,20 +44,41 @@ function App() {
     setDice(tempDice);
   };
 
+  // Switch to next player and reset all dice and amount of rolls left
+  const switchToNextPlayer = () => {
+    const tempDice = [...dice];
+
+    for (let i = 0; i < tempDice.length; i++) {
+      if (tempDice[i].saved === true) {
+        tempDice[i].saved = false;
+      }
+    }
+
+    setDice(tempDice);
+    currentPlayer === 1 ? setCurrentPlayer(2) : setCurrentPlayer(1);
+    setRollsLeft(3);
+  };
+
   return (
     <div>
-      <button onClick={rollDice}>Roll dice</button>
+      <h2>Player {currentPlayer}'s turn</h2>
+      <h2>Rolls left: {rollsLeft}</h2>
+      <button disabled={rollsLeft === 0} onClick={rollDice}>
+        Roll dice
+      </button>
+      <button onClick={switchToNextPlayer}>Next Player</button>
       <div>
         Unsaved dice:
-        {dice.map((die) => {
-          if (die.saved === false) {
-            return (
-              <button key={die.id} id={die.id} onClick={saveUnsaveDie}>
-                {die.value}
-              </button>
-            );
-          } else return null;
-        })}
+        {rollsLeft < 3 &&
+          dice.map((die) => {
+            if (die.saved === false) {
+              return (
+                <button key={die.id} id={die.id} onClick={saveUnsaveDie}>
+                  {die.value}
+                </button>
+              );
+            } else return null;
+          })}
       </div>
       <div>
         Saved dice:
